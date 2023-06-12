f2514688f78c417a867ac2215f0fec03a45d1f92
BrageNTNU <bragehk@stud.ntnu.no>
2023-06-12 21:50:10 +0200

    All tasks finished

--- task2/README.md
+++ task2/README.md
@@ -1,6 +1,6 @@
 ## Oppgave 2
 Du vil få en segmentation fault: Process finished with exit code 139 (interrupted by signal 11: SIGSEGV).
-Nullpointer er utenfor minneadressen som programmet har tilgang til. 
+Nullpointer er utenfor minneadressen som programmet har tilgang til og det er derfor ikke mulig å bruke string copy.
 
 ## Oppgave 3
 Hva kan gå galt her?
@@ -16,5 +16,12 @@ while (*pointer != search_for) {
 ```
 
 Hvis brukeren skriver inne et ord som har mer enn 5 tegn, kommer en segmentation fault. Dette er fordi 
-listen med bokstaver kan makse være 4. Grunnen til at lengden kan være 4, og ikke 5 er fordi 
+listen med bokstaver kan maks være 4. Grunnen til at lengden kan være 4, og ikke 5 er fordi når cin 
+blir brukt, legges det til et ekstra tegn, "\0", i slutten av strengern. Det er mulig å bruke
+cin.getline() for å komme unna dette.
+
+Hvis teksten man skriver inn ikke inneholder en "e", vil det være en uendelig loop som til slutt går
+ til en ugyldig minneadresse.
+
+
 
--- task2/main.cpp
+++ task2/main.cpp
@@ -2,7 +2,16 @@
 
 using namespace std;
 
+int find_sum(const int *table, int length) {
+    int sum = 0;
+    for(int i = 0; i < length; i++) {
+        sum += *table++;
+    }
+    return sum;
+}
+
 int main() {
+    // ------ OPPGAVE 1 --------
     int i = 3;
     int j = 5;
     int *p = &i;
@@ -16,8 +25,8 @@ int main() {
     *q += 4;
     *q = *p + 1;
     p = q;
-    cout << "p pointer: " << *p << " p value: " << p << endl;
-    cout << "q pointer: " << *q << " q value: " << q << endl;
+    cout << "p pointer value " << *p << " p address: " << p << endl;
+    cout << "q pointer value: " << *q << " q address: " << q << endl;
 
     char text[5];
     char *pointer = text;
@@ -28,5 +37,42 @@ int main() {
         *pointer = search_for;
         pointer++;
     }
+
+    // ------- OPPGAVE 4 --------
+    int a = 5;
+    int b; // Kan ikke definere minneadresse. Mulig å initialisere en variabel eller peker i stedet.
+    int *c;
+    c = &b;
+    a = b + *c; // Skjønner ikke helt hva programmet vil her, men setter verdien av a til å være lik verdien av b og
+    // verdien av c. Jeg regner med at man ikke har lyst til å sette minneadressen til en sum av de andre
+    // minneadressene ettersom det kan lett føre til segmentation fault fordi minneadressen blir utenfor det programmet
+    // har tilgang til.
+    b = 2; // Er litt merkelig å prøve å sette minneadressen til 2. Setter heller vaiabelen til 2.
+
+
+    // ------- OPPGAVE 5 -------
+    cout << "\n----- Oppgave 5 -----" << endl;
+    double number = 10.2;
+    cout << "Number: " << number << endl;
+    double *numPointer = &number;
+    *numPointer = 3.3;
+    cout << "Number: " << number << endl;
+    double &referanse = number;
+    referanse = 6.9;
+    cout << "Number: " << number << endl << endl;
+
+    // ----- OPPGAVE 6 ------
+    cout << "----- Oppgave 6 -----" << endl;
+    int table[20];
+    for(int i = 0; i < 20; i++) {
+        *(table+i) = i+1;
+        cout << *(table+i) << " ";
+    }
+    cout << "\nSum: " << find_sum(table, 10) << endl;
+    cout << "Sum: " << find_sum(table+10, 5) << endl;
+    cout << "Sum: " << find_sum(table+15, 5) << endl;
+
+
+
     return 0;
 }
